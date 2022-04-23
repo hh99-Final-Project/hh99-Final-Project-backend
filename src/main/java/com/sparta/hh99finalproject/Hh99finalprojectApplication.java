c16a03355835181efbc209f577ba76308cc01bc3
jaeyoungjang2 <lion85488@gmail.com>
2022-04-23 11:24:30 +0900

    add readme

---
+++ src/main/java/com/sparta/hh99finalproject/Hh99finalprojectApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.hh99finalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hh99finalprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hh99finalprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/hh99finalproject/Hh99finalprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.hh99finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hh99finalprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
