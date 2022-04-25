409457d5f0daef9e446be0e32ed152d815cffa1a
jaeyoungjang2 <lion85488@gmail.com>
2022-04-25 17:23:04 +0900

    feat: 구글 로그인 추가

--- src/main/java/com/sparta/hh99finalproject/repository/UserRepository.java
+++ src/main/java/com/sparta/hh99finalproject/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.sparta.hh99finalproject.repository;
 
 import com.sparta.hh99finalproject.domain.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByNickname(String nickname);
+
+    Optional<User> findByUsername(String email);
 }
