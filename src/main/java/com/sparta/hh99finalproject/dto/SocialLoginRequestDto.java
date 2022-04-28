e04ef601f194e215a0fea066eb19e6ed3006947d
jaeyoungjang2 <lion85488@gmail.com>
2022-04-28 23:09:29 +0900

    소셜 로그인 -> 프론트로 부터 토큰을 받아서 로그인을 하도록 설정 변경 기존 코드는 다른 사람들이 조금 더 보고자 하여 추후 삭제 예정

---
+++ src/main/java/com/sparta/hh99finalproject/dto/SocialLoginRequestDto.java
@@ -0,0 +1,12 @@
+package com.sparta.hh99finalproject.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SocialLoginRequestDto {
+
+    private String jwtToken;
+
+}
